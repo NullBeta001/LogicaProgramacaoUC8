38585c86c68a4d59a72f9325bf73d7c95e512488
NullBeta001 <fabiooli221297@gmail.com>
2022-01-20 22:48:46 -0300

    Codando cadastro de participantes do evento sem input

--- NoInput.js
+++ NoInput.js
@@ -5,6 +5,14 @@
 //Data do evento = Quinta-feira, 03 de Fevereiro de 2022.
 //
 
+let listaDeParticipantes = ["Helena", "Fábio", "Pedro", "Paulo", "Fernando", "Vinícius", "Wilson", "Nelson", "Carlos"];
+let participantes = listaDeParticipantes.length;
+let listaPart = listaDeParticipantes.length
+
+let quantidaDeParticipantes = listaDeParticipantes.length
+
+let idadeParticipante = 17
+
 let = dataAtual = new Date();
 function formatData(dataAtual){
     let newDate = new Date(dataAtual);
@@ -13,21 +21,27 @@ function formatData(dataAtual){
 console.log("Data atual", formatData(dataAtual))
 
 let = dataDoEvento = new Date("2022-02-03")
-if (dataAtual > dataDoEvento) {
-    console.log("Data de cadastro inválda!")
+if (dataAtual < dataDoEvento) {
+    console.log("Data de cadastro válida!")
+    if (idadeParticipante >= 18){
+        console.log("Idade permitida!")
+        console.log("Lista de Participantes:")
+        for (let indice = 0; indice < listaPart; indice++) {
+            const listaTotal = listaDeParticipantes[indice];
+            console.log(listaTotal)
+        }
+            console.log("Quantidade total de participantes", participantes);
+            if (quantidaDeParticipantes < 100) {
+            console.log("Seu cadastro foi concluido com sucesso!")
+        }else{
+            console.log("Quantidade máxima de participantes atingida, cadastro cancelado!")
+        }
+    }else{
+        console.log("Idade minima de 18 anos, cadastro cancelado!")
+    }
 }else{
-    console.log("Data válida")
+    console.log("Data de cadastro inválida, cadastro cancelado!")
 }
 
-let = dataNascimento = new Date("2005-12-22")
-let = dataAtual = new Date();
-function formatData(dataNascimento){
-    let newDate = new Date(dataNascimento);
-    return `${newDate.getDate()+1}/${newDate.getMonth()+1}/${newDate.getFullYear()}`
-}
-console.log("Data de Nascimento", formatData(dataNascimento))
-if (dataAtual - dataNascimento >= 18){
-    console.log("Idade permitida!")
-}else{
-    console.log("Idade mínima 18 anos, cadastro cancelado!")
-}
+//Fim do código de cadastro de participantes do envento.
+//Sem input.
