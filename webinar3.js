97e847df255105afc6468722933b7828b97823b2
NullBeta001 <fabiooli221297@gmail.com>
2022-01-25 20:53:44 -0300

    Atividade 3 webinar 25.01

---
+++ webinar3.js
@@ -0,0 +1,32 @@
+//Listando as publicações da minha rede social favorita
+
+//for (let indice = 0; indice < listaDeNoticias.length; indice++) {
+//    const elementoAtualDaLista = listaDeNoticias [indice];
+//    console.log(elementoAtualDaLista)
+//}
+
+//Laço contado -> For
+
+//Enquanto o contador for maior que o 5, irá adicionar um número
+//Como contador=6, entra em um loop infinto
+//Para sair do bug, "Ctrl + C"
+//for (let contador = 6; contador > 5; contador++) {
+//    console.log(contador)
+
+let listaDeAlunos = ["Pedro", "João", "Denise", "Jéssica", "Fábio"]
+
+for (let contador = 0; contador < 5; contador++) {
+    console.log(`Boa tarde ${listaDeAlunos[contador]}`)
+}
+
+console.log("----/----")
+
+for (let indice = 0; indice < 11; indice++) {
+    console.log(3 * indice)
+}
+
+console.log("----/----")
+
+for (let numero = 10; numero > 0; numero--) {
+    console.log(numero)
+}
