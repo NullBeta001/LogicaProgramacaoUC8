176ce76c34d3e5c591f4e65b8ef382b0337b606b
NullBeta001 <fabiooli221297@gmail.com>
2022-01-25 20:06:26 -0300

    Atividade 1 webinar 25.01

---
+++ webinar1.js
@@ -0,0 +1,21 @@
+//Codificando o jantar de hoje
+const alimentoSelecionado = "pedra";
+
+//Modo 1
+switch (alimentoSelecionado) {
+    case "arroz":
+        console.log ("vamos jantar " + alimentoSelecionado);
+        break;
+
+    case "batata":
+        console.log ("vamos jantar " + alimentoSelecionado)
+        break;
+
+    case "lasanha":
+        console.log ("vamos jantar " + alimentoSelecionado)
+        break;
+        
+    default:
+        console.log ("o item não consta na lista")
+        break;
+}
