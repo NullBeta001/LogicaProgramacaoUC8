ef698267fc77571a5de17c7a8fa8126301badc35
NullBeta001 <fabiooli221297@gmail.com>
2022-01-25 20:19:41 -0300

    Atividade 2 webinar 25.01

---
+++ webinar2.js
@@ -0,0 +1,15 @@
+//Codificando o jantar de hoje
+const alimentoSelecionado = "arroz";
+
+//Modo 2
+
+
+if (alimentoSelecionado === "arroz") {
+    console.log ("vamos jantar " + alimentoSelecionado);
+} else if (alimentoSelecionado === "lasanha") {
+    console.log ("vamos jantar " + alimentoSelecionado);
+} else if (alimentoSelecionado === "batata") {
+    console.log ("vamos jantar " + alimentoSelecionado);
+} else {
+    console.log ("o item não consta na lista")
+}
