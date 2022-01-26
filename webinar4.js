7aa03104cfdc240c0b41263b01e87e67cea4fa6f
NullBeta001 <fabiooli221297@gmail.com>
2022-01-25 21:32:25 -0300

    Finalização de atividades

---
+++ webinar4.js
@@ -0,0 +1,11 @@
+//Laço Condicional
+
+let nomeUsuario = "Fábio"
+
+let soma = 0
+
+while(soma < 10) { 
+    console.log("Boa tarde Fábio", soma)
+
+    soma++
+}
---
+++ webinar5.js
@@ -0,0 +1,12 @@
+//Laço Condicional
+
+let nomeUsuario = "Fábio"
+
+while(nomeUsuario == "Fábio") { 
+    console.log("Boa tarde Fábio")
+
+    nomeUsuario = "Valter"
+}
+
+//Neste caso o terminal exibirá o comando infinitamente, pelo fato de não especificado o fim.
+
